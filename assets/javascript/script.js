8eb684ea9b96f4b1ee0ed253162851fdac9cb02e
Adriano Rodrigues <a.rodrigues.sobrinho4@gmail.com>
2026-03-09 23:22:55 -0300

    feat: Salvamento no LocalStorage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,31 +3,59 @@ const inputName = document.getElementById("link-name");
 const inputUrl = document.getElementById("link-url");
 const linksContainer = document.getElementById("links-container");
 
-/*
-    LÓGICA:
-    1. Escutar envio do formulário
-    2. Impedir recarregamento
-    3. Pegar dados digitados
-    4. Criar elementos HTML
-    5. Configurar o link
-    6. Adicionar na página
-    7. Limpar formulário
-*/
+// Transforma objeto em texto para armazenar
+function saveLinks(links) {
+    localStorage.setItem("links", JSON.stringify(links));
+}
+
+// Transforma texto em objeto novamente
+function loadLinks() {
+    const storedLinks = localStorage.getItem("links");
+
+    if (storedLinks) {
+        return JSON.parse(storedLinks);
+    }
+
+    return [];
+}
+
+function renderLinks() {
+    linksContainer.innerHTML = "";
+
+    const links = loadLinks();
+
+    links.forEach(function(link) {
+        const linkItem = document.createElement("div");
+        const linkElement = document.createElement("a");
+
+        linkElement.textContent = link.name;
+        linkElement.href = link.url;
+        linkElement.target = "_blank";
+
+        linkItem.appendChild(linkElement);
+
+        linksContainer.appendChild(linkItem);
+    });
+}
+
 form.addEventListener("submit", function(event){
     event.preventDefault();
 
     const name = inputName.value;
     const url = inputUrl.value;
 
-    const linkItem = document.createElement("div")
-    const linkElement = document.createElement("a");
+    const links = loadLinks();
 
-    linkElement.textContent = name;
-    linkElement.href = url;
-    linkElement.target = "_blank";
+    links.push({
+        name: name,
+        url: url
+    });
 
-    linkItem.appendChild(linkElement);
-    linksContainer.appendChild(linkItem);
+    saveLinks(links);
+
+    renderLinks();
 
     form.reset();
 });
+
+renderLinks();
